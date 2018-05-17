16f15b31944fff51d05de2572a74308ba5e25d2e
JaeJoon Jung <rgbi3307@nate.com>
2018-05-17 13:21:05 +0900

    commit first on 2018-05-17

---
+++ macro/xlen.s
@@ -0,0 +1,27 @@
+	.file	"xlen.c"
+	.option nopic
+	.section	.rodata
+	.align	3
+.LC0:
+	.string	"XLEN = %d\n"
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-16
+	sd	ra,8(sp)
+	sd	s0,0(sp)
+	addi	s0,sp,16
+	li	a1,64
+	lui	a5,%hi(.LC0)
+	addi	a0,a5,%lo(.LC0)
+	call	printf
+	li	a5,0
+	mv	a0,a5
+	ld	ra,8(sp)
+	ld	s0,0(sp)
+	addi	sp,sp,16
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (GNU) 7.2.0"
