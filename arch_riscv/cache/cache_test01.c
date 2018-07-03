d0c372513121319f34cd451254fdde8d15dd0fba
JaeJoon Jung <rgbi3307@nate.com>
2018-07-03 20:01:10 +0900

    commit cache on 2018-07-03

--- arch_riscv/cache/cache_test01.c
+++ arch_riscv/cache/cache_test01.c
@@ -3,6 +3,23 @@
     editted by: Jung-JaeJoon <rgbi3307@nate.com> on the www.kernel.bz
     function: cache test
               objdump -d -t -r a.out
+
+    Refer:
+    https://patchwork.kernel.org/patch/10067051/
+    Signed-off-by: Andrew Waterman <andrew@sifive.com>
+    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>
+    ---
+     arch/riscv/include/asm/cacheflush.h    |  6 ++++++
+     arch/riscv/include/asm/vdso-syscalls.h | 28 ++++++++++++++++++++++++++++
+     arch/riscv/include/asm/vdso.h          |  4 ++++
+     arch/riscv/kernel/sys_riscv.c          | 32 ++++++++++++++++++++++++++++++++
+     arch/riscv/kernel/syscall_table.c      |  2 ++
+     arch/riscv/kernel/vdso/Makefile        |  1 +
+     arch/riscv/kernel/vdso/flush_icache.S  | 31 +++++++++++++++++++++++++++++++
+     arch/riscv/kernel/vdso/vdso.lds.S      |  1 +
+     8 files changed, 105 insertions(+)
+     create mode 100644 arch/riscv/include/asm/vdso-syscalls.h
+     create mode 100644 arch/riscv/kernel/vdso/flush_icache.S
 */
 
 ///arch/riscv/include/asm/sbi.h
